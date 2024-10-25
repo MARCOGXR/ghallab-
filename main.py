7eda2d87d990d1784d4bab81836f1f51a8adf405
MARCOGXR <ghalab8989@gmail.com>
2024-10-25 19:09:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+class ghallab:
+    def ghallab(self):
+        number = int(input("enter the number :"))
+        if number % 2 == 0:
+            print('yes')
+        else:
+            print('')
+
+
+ghallab_display = ghallab()
+ghallab_display.ghallab()
